75f8474d550821bc96b8d7dd11a778203413fbea
jhonarley <johnarleyss@hotmail.com>
2024-01-19 17:27:46 -0300

    correção de bugs

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
 let key = "aa4651314bb3e30ea4fbee0f9dba9004"
 function colocarNaTela(dados){
 console.log(dados)
-document.querySelector(".city").innerHTML = "Tempo em" + dados.name
+document.querySelector(".local").innerHTML = "Tempo em" + dados.name
 document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
-document.querySelector(".city").innerHTML = dados.weather[0].description
-document.querySelector(".icone").scr = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+document.querySelector(".description").innerHTML = dados.weather[0].description
+document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
 }
-async function buscarCity(city){
+async function buscarCity(local){
     let dados = await fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" + 
-        city +
+        local +
          "&appid="+
          key +
          "&lang=pt_br" +
-         "&unists=metric"
+         "&units=metric"
         )
         .then(resposta => resposta.json())
         colocarNaTela(dados)
@@ -21,7 +21,7 @@ async function buscarCity(city){
 }
 
 function cliqueiNoBotao() {
-let city = document.querySelector(".input-city").value
+let local = document.querySelector(".input-city").value
 
-buscarCity(city)
+buscarCity(local)
 }
